873824578ed097a885f73a3c4ab3a4b31497a46f
Ilias Ibrahim <iliasone@yahoo.com>
2020-12-29 10:09:30 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-text">
-          Under Construction! Stay tuned....
+          Under Construction! Stay tuned..
         </p>
         
       </header>
